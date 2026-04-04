96b3a7b51760de89a4784cbeb6394da5ead506db
TornovDutta <tornovdutta@gmail.com>
2026-04-04 22:20:11 +0530

    upgrade to 3.4.4 spring boot version

--- src/main/java/com/campus/whisper/config/RedisConfig.java
+++ src/main/java/com/campus/whisper/config/RedisConfig.java
@@ -1,20 +1,27 @@
 package com.campus.whisper.config;
 
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import tools.jackson.databind.ObjectMapper;
 
 import java.time.Duration;
 
 @Configuration
-@EnableCaching
 public class RedisConfig {
 
     @Bean
     public RedisCacheConfiguration cacheConfiguration() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
         return RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(60))
-                .disableCachingNullValues();
+                .entryTtl(Duration.ofMinutes(10))
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new GenericJacksonJsonRedisSerializer(objectMapper)
+                        )
+                );
     }
 }
